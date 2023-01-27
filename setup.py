97017360d71d7d326fd455ffdaf7874621c1aa1f
BlueSchnabeltier <97361820+BlueSchnabeltier@users.noreply.github.com>
2023-01-27 19:50:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 
 class InstallPlaywrightDrivers(install):
     def run(self):
-        check_output(f"{executable} -m install playwright", shell=True)
+        check_output([executable, "-m", "install", "playwright"])
         install.run(self)
 
 setup(name="playwright-driver-autoinstall", install_requires=["playwright"], cmdclass={"install": InstallPlaywrightDrivers})
