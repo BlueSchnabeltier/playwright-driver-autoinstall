41f01c6d36df2c5a2058b2e3b741eadc90e6cff1
BlueSchnabeltier <97361820+BlueSchnabeltier@users.noreply.github.com>
2023-04-16 14:55:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from setuptools.command.install import install
 
 class InstallPlaywrightDrivers(install):
     def run(self):
-        call([executable, "-m", "playwright", "install"])
+        call([executable, "-m", "playwright", "install", "chromium"])
+        call([executable, "-m", "playwright", "install", "firefox"])
+        call([executable, "-m", "playwright", "install", "webkit"])
         install.run(self)
 
 setup(name="playwright-driver-autoinstall", version="1.0", install_requires=["playwright"], cmdclass={"install": InstallPlaywrightDrivers})
